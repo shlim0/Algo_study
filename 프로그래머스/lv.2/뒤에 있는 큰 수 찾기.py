8e50a7a4e3bfb9e8384fa73b38a7fbf985d00044
jfknfj920 <jfknfj920@naver.com>
2024-06-07 23:36:57 +0900

    lv2_Stack

--- 프로그래머스/lv.2/뒤에 있는 큰 수 찾기.py
+++ 프로그래머스/lv.2/뒤에 있는 큰 수 찾기.py
@@ -4,9 +4,7 @@ def solution(numbers):
 
     for i in range(len(numbers)):
         while stack and numbers[stack[-1]] < numbers[i]:
-            print(stack, numbers[stack[-1]], numbers[i])
             answer[stack.pop()] = numbers[i]
-        print(i)
         stack.append(i)
 
     return answer
