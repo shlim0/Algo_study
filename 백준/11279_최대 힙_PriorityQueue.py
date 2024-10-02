ca3d81dcc30d99a7fa91a1e3b40caa356d54f0fa
shlim <jfknfj920@naver.com>
2024-10-02 19:06:58 +0900

    S2_PriorityQueue

---
+++ 백준/11279_최대 힙_PriorityQueue.py
@@ -0,0 +1,16 @@
+# 3 min
+import sys
+from queue import PriorityQueue
+
+N = int(input())
+pq = PriorityQueue()
+
+for _ in range(N):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        if pq.empty():
+            print(0)
+        else:
+            print(-pq.get())
+    elif x > 0:
+        pq.put(-x)
